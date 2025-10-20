db39e6b271aefbb99d2bf535cdbf2a899ccfe5ef
Salehin Khan <saleheen04@gmail.com>
2025-10-20 13:52:41 +0600

    gffggfgf

--- services/stripeService.js
+++ services/stripeService.js
@@ -30,7 +30,6 @@ export async function createCheckoutSession(priceId, name, email, productId, pla
       name
     },
 
-    // 1. ADD THIS LINE: Force billing address collection
     billing_address_collection: 'required', // or 'auto'
     
     // 💡 REQUIRED: Enable automatic tax calculation (Stripe Tax)
